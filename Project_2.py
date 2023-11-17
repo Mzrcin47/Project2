c8239dd2fe1e239f06cdb9497cabcbb1ea58a2a3
Mzrcin47 <marcin.pawluch@torontomu.ca>
2023-11-16 20:45:08 -0500

    No errors so as of now(problem arises from compile)

--- Project_2.py
+++ Project_2.py
@@ -4,13 +4,16 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.keras import datasets, layers, models
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+
 import os
 from PIL import Image
 
 
 ' Step 1 '
 
-desired_shape = (100, 100, 3)
+#desired_shape = (100, 100, 3)
+width,height,channels= 100,100,3 
 
 #absolute_path= os.path.dirname(__file__)
 
@@ -18,9 +21,9 @@ train_data_dir='./Data/Train'
 test_data_dir='./Data/Test'
 validation_dir='./Data/Validation'
 
-traindataset= tf.keras.utils.image_dataset_from_directory(
-    validation_dir
-    )
+#traindataset= tf.keras.utils.image_dataset_from_directory('
+ #   validation_dir
+  #  )
 
 #plt.figure(figsize=(10,10))
 #for images, labels in train_data_dir.take(1):
@@ -32,9 +35,9 @@ traindataset= tf.keras.utils.image_dataset_from_directory(
 #validation_dir=os.path.join(absolute_path, 'Data', 'Validation')
 #extracted_and_preprocessed_images = extract_and_preprocess_images_from_folder(data_dir)
 
-train_images = os.listdir(train_data_dir)
-validation_images = os.listdir(validation_dir)
-test_images = os.listdir(test_data_dir)
+#train_images = os.listdir(train_data_dir)
+#validation_images = os.listdir(validation_dir)
+#test_images = os.listdir(test_data_dir)
 
 'data augmantation and imagedatafounddirectory'
 
@@ -53,6 +56,12 @@ train_generator = train_datagen.flow_from_directory(
 
 validation_datagen = ImageDataGenerator(rescale=1.0/255.0)
 
+calidation_gen = train_datagen.flow_from_directory(
+    train_data_dir,         
+    target_size=(100, 100, 3), 
+    batch_size=32,          
+    class_mode='categorical' 
+    )
 
 'Step 2'
 
@@ -70,9 +79,9 @@ model1.add(layers.Dense(10))
 
 model1.summary()
 
-model1.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
+#model1.compile(optimizer='adam',
+ #             loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+  #            metrics=['accuracy'])
 
-history = model1.fit(train_images, epochs=10, validation_data=validation_datagen)
+#history = model1.fit(train_images, epochs=10, validation_data=validation_datagen)
 
