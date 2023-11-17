6133a58c5637063bdd21e21e175fab3dc7b0886a
Mzrcin47 <marcin.pawluch@torontomu.ca>
2023-11-16 19:40:41 -0500

    Trying to find an error

--- Project_2.py
+++ Project_2.py
@@ -12,11 +12,24 @@ from PIL import Image
 
 desired_shape = (100, 100, 3)
 
-absolute_path= os.path.dirname(__file__)
+#absolute_path= os.path.dirname(__file__)
 
-train_data_dir= os.path.join(absolute_path, 'Data', 'Train')
-test_data_dir=os.path.join(absolute_path, 'Data', 'Test')
-validation_dir=os.path.join(absolute_path, 'Data', 'Validation')
+train_data_dir='./Data/Train'
+test_data_dir='./Data/Test'
+validation_dir='./Data/Validation'
+
+traindataset= tf.keras.utils.image_dataset_from_directory(
+    validation_dir
+    )
+
+#plt.figure(figsize=(10,10))
+#for images, labels in train_data_dir.take(1):
+ #   plt.imshow(images[1].numpy().asytype("unit8"))
+ #   plt.axis("off")
+
+#train_data_dir= os.path.join(absolute_path, 'Data', 'Train')
+#test_data_dir=os.path.join(absolute_path, 'Data', 'Test')
+#validation_dir=os.path.join(absolute_path, 'Data', 'Validation')
 #extracted_and_preprocessed_images = extract_and_preprocess_images_from_folder(data_dir)
 
 train_images = os.listdir(train_data_dir)
@@ -33,17 +46,18 @@ train_datagen = ImageDataGenerator(
 
 train_generator = train_datagen.flow_from_directory(
     train_data_dir,         
-    target_size=(100, 100), 
+    target_size=(100, 100, 3), 
     batch_size=32,          
     class_mode='categorical' 
     )
 
 validation_datagen = ImageDataGenerator(rescale=1.0/255.0)
 
+
 'Step 2'
 
 model1 = models.Sequential()
-model1.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+model1.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(100, 100, 3)))
 model1.add(layers.MaxPooling2D((2, 2)))
 model1.add(layers.Conv2D(64, (3, 3), activation='relu'))
 model1.add(layers.MaxPooling2D((2, 2)))
@@ -60,6 +74,5 @@ model1.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
-history = model1.fit(train_images, epochs=10, 
-                 validation_data=(test_images))
+history = model1.fit(train_images, epochs=10, validation_data=validation_datagen)
 
