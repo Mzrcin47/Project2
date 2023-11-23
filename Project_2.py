0bad5e962190996701ee9d273bc64d478e06b598
Mzrcin47 <marcin.pawluch@torontomu.ca>
2023-11-23 17:55:03 -0500

    Testing

--- Project_2.py
+++ Project_2.py
@@ -84,9 +84,9 @@ validation_gen = validation_datagen.flow_from_directory(
 # print(history.history)
 
 model2 = models.Sequential()
-model2.add(layers.Conv2D(64, (3, 3), activation='relu', input_shape=(desired_shape)))
+model2.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(desired_shape)))
 model2.add(layers.MaxPooling2D((2, 2)))
-model2.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model2.add(layers.Conv2D(32, (3, 3), activation='relu'))
 model2.add(layers.MaxPooling2D((2, 2)))
 model2.add(layers.Conv2D(128, (3, 3), activation='relu'))
 model2.add(layers.Dropout(0.6))
