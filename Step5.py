833c01b3a1625aaabed9bc12b9ca44f884401b99
Mzrcin47 <marcin.pawluch@torontomu.ca>
2023-11-26 14:37:51 -0500

    Formatting

--- Step5.py
+++ Step5.py
@@ -21,20 +21,15 @@ class_labels = {0: "Small", 1: "Medium", 2: "Large", 3: "None"}
 class_labelsperc = {0: "Small Crack", 1: "Medium Crack", 2: "Large Crack", 3: "No Crack"}
 
 for img_path in testing_images:
+    
     img, img_array = preprocess_image(img_path)
-
     predictions = model.predict(img_array)
-
     predicted_class = np.argmax(predictions)
-
     actual_class = img_path.split("/")[-2]
-
     percentages = [f"{class_labelsperc[i]}: {predictions[0, i] * 100:.1f}%" for i in range(len(class_labelsperc))]
-
     plt.imshow(img)
     plt.title(f"True Crack Label: {actual_class}\nPredicted Crack Classification Label: {class_labels[predicted_class]}")
 
-    # Display class percentages on the image (bottom right corner)
     for i, text in enumerate(percentages):
         plt.text(95, 95 - i * 5, text, fontsize=11, color='green', ha='right', va='top')
 
